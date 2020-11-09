0cdf1659e6034b271e06ad2d17053718eb0dbdc5
potados99 <potados99@gmail.com>
2020-11-09 19:05:51 +0900

    Add some comments.

--- mvvm/src/main/java/org/potados/mvvm/MainViewModel.kt
+++ mvvm/src/main/java/org/potados/mvvm/MainViewModel.kt
@@ -4,12 +4,16 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+// 뷰모델
 class MainViewModel : ViewModel() {
+
+    // 모델
+    private val textRepository = TextRepository()
 
     private val _myText = MutableLiveData("Hello!")
     val myText: LiveData<String> = _myText
 
     fun onButtonClick() {
-        _myText.value += " yeah"
+        _myText.value += textRepository.getText()
     }
 }
---
+++ mvvm/src/main/java/org/potados/mvvm/TextRepository.kt
@@ -0,0 +1,12 @@
+package org.potados.mvvm
+
+class TextRepository {
+
+    fun getText(): String {
+
+
+
+
+        return "hehe"
+    }
+}
