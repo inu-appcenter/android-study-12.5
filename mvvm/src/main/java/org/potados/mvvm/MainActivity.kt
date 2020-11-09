7e2f2f1dda94603433379ca568230171f30b6672
potados99 <potados99@gmail.com>
2020-11-09 18:35:16 +0900

    Add some comments.

--- mvvm/src/main/java/org/potados/mvvm/MainActivity.kt
+++ mvvm/src/main/java/org/potados/mvvm/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     private val TAG = "여길좀 봐줘요~!~!!!!"
     private val viewModel: MainViewModel by viewModels()
 
+    // 맨날씀
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -30,11 +31,13 @@ class MainActivity : AppCompatActivity() {
         Log.i(TAG, "onStart")
     }
 
+    // 자주씀
     override fun onResume() {
         super.onResume()
         Log.i(TAG, "onResume")
     }
 
+    // 자주씀
     override fun onPause() {
         super.onPause()
         Log.i(TAG, "onPause")
@@ -45,6 +48,7 @@ class MainActivity : AppCompatActivity() {
         Log.i(TAG, "onStop")
     }
 
+    // 가끔?씀
     override fun onDestroy() {
         super.onDestroy()
         Log.i(TAG, "onDestroy")
