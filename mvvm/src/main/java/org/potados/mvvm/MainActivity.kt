512ed707128a278504d6d57df58e290191156e6b
potados99 <potados99@gmail.com>
2020-11-09 18:05:45 +0900

    Add some lifecycle log.

--- mvvm/src/main/java/org/potados/mvvm/MainActivity.kt
+++ mvvm/src/main/java/org/potados/mvvm/MainActivity.kt
@@ -1,14 +1,14 @@
 package org.potados.mvvm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.util.Log
 import androidx.activity.viewModels
-import kotlinx.android.synthetic.main.activity_main.view.*
+import androidx.appcompat.app.AppCompatActivity
 import org.potados.mvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private val TAG = "여길좀 봐줘요~!~!!!!"
     private val viewModel: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,5 +21,32 @@ class MainActivity : AppCompatActivity() {
             lifecycleOwner = this@MainActivity
             setContentView(root)
         }
+
+        Log.i(TAG, "onCreate")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i(TAG, "onStart")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Log.i(TAG, "onResume")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.i(TAG, "onPause")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.i(TAG, "onStop")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.i(TAG, "onDestroy")
     }
 }
