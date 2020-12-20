3a111874833314cf49becd2259c4cf09cde80121
potados99 <potados99@gmail.com>
2020-12-20 18:34:19 +0900

    Use ViewBinding.

--- view_binding/src/main/java/org/potados/view_binding/MainActivity.kt
+++ view_binding/src/main/java/org/potados/view_binding/MainActivity.kt
@@ -2,10 +2,18 @@ package org.potados.view_binding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import org.potados.view_binding.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // binding.myTextView
     }
 }
